bb7b52eefa54d1f1376e68ddec7566851b88a60e
Clare <CTuch19@holynames-sea.org>
2018-09-15 12:15:16 -0700

    initial commit

---
+++ 5588ScoutingApp/5588ScoutingApp/SecondViewController.designer.cs
@@ -0,0 +1,28 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace _5588ScoutingApp
+{
+    [Register ("SecondViewController")]
+    partial class SecondViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton b1 { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (b1 != null) {
+                b1.Dispose ();
+                b1 = null;
+            }
+        }
+    }
+}
